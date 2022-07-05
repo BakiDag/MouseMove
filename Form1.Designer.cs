917fcf6d528c516ef965d0f63d7bb2ab8342f95e
BakiDag <73464615+BakiDag@users.noreply.github.com>
2022-07-05 22:28:39 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace MouseMove
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 12);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            this.btn_Start.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btn_Start_KeyDown);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(12, 46);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.Text = "stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Strg+X to exit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ScreenWidth: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ScreenHeight: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "MouseX:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(171, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "MouseY:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Stoppuhr:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(8, 149);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 15);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Alt+F4 to exit";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(32, 118);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 15);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "or";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 277);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+
+        #endregion
+
+        //private System.Windows.Forms.Button button1;
+        //private System.Windows.Forms.Button button2;
+        //private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMove
+{
+    public partial class Form1 : Form
+    { 
+        System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+        TimeSpan timeSpan;
+        bool breaker = true;
+        double width = Screen.PrimaryScreen.Bounds.Width - 20;
+        Random rand = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            label2.Text = "ScreenWidth: " + Screen.PrimaryScreen.Bounds.Width.ToString();
+            label3.Text = "ScreenHeight: " + Screen.PrimaryScreen.Bounds.Height.ToString();            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        
+        public async void btn_Start_Click(object sender, EventArgs e)
+        {
+            breaker = true;
+            Loop();
+        }
+        private void btn_Start_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        public bool BreakLoop()
+        {
+            //breaker = _breaker;
+            breaker = false;
+            return breaker;
+        }
+        public async Task Loop()
+        {
+            s.Reset();
+            s.Start();
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            await Task.Run(() =>
+            {
+                while (breaker ==true)
+                {
+                    if (breaker == true)
+                    {
+                        int YAchse = Cursor.Position.Y;
+                        if (Cursor.Position.Y!=YAchse || Cursor.Position.Y > YAchse || Cursor.Position.Y < YAchse)
+                        {
+                               
+                            breaker = false;
+                            s.Stop();
+                            break;
+                        }
+                        
+                        if (width == Convert.ToDouble(Cursor.Position.X)) //erreichen des Bildschirmrandes
+                        {
+                            //Cursor.Position = new Point(0, Cursor.Position.Y);                        
+                            Cursor.Position = new System.Drawing.Point(0, rand.Next(1, Screen.PrimaryScreen.Bounds.Height));
+                            Thread.Sleep(10);
+
+                        }
+                        else
+                        {
+                            Cursor.Position = new Point(Cursor.Position.X + 1, Cursor.Position.Y);
+                            Thread.Sleep(10);
+
+                        }
+                    }
+                    else
+                    {
+                        s.Stop();
+                        
+                        
+                        break;
+                    }
+                }
+            });
+        }       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label6.Text = timeSpan.Hours.ToString("00") + ":" + timeSpan.Minutes.ToString("00") + ":" + timeSpan.Seconds.ToString("00");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.X || e.Alt && e.KeyCode==Keys.F4)//it should stop now
+            {
+                BreakLoop();
+                this.Close();
+            }            
+        }
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            BreakLoop();
+            s.Stop();           
+            timeSpan = s.Elapsed;
+            s.Reset();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = "MouseX: " + Cursor.Position.X.ToString();
+            label5.Text = "MouseY: " + Cursor.Position.Y.ToString();            
+        }
+    }
+}
